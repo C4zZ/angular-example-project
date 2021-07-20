75f75ddc466c1ad10874ddf6f3b8207f9b149a61
David <david@pop-os.localdomain>
2021-07-20 19:33:08 +0200

    add ShoppingListComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
 import { ShoppingListComponentComponent } from './shopping-list-component/shopping-list-component.component';
 import { RecipeBookComponentComponent } from './recipe-book-component/recipe-book-component.component';
+import { ShoppingListEditComponentComponent } from './shopping-list-edit-component/shopping-list-edit-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponentComponent,
     ShoppingListComponentComponent,
-    RecipeBookComponentComponent
+    RecipeBookComponentComponent,
+    ShoppingListEditComponentComponent
   ],
   imports: [
     BrowserModule

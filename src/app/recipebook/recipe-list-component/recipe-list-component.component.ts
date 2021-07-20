226e4c8200936efc0577080a9bd05ca4940e9065
David <david@pop-os.localdomain>
2021-07-20 20:26:54 +0200

    add Recipe

--- src/app/recipebook/recipe-list-component/recipe-list-component.component.ts
+++ src/app/recipebook/recipe-list-component/recipe-list-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../Recipe";
 
 @Component({
   selector: 'app-recipe-list-component',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list-component.component.css']
 })
 export class RecipeListComponentComponent implements OnInit {
+  recipies: Recipe[];
 
-  constructor() { }
+  constructor() {
+    this.recipies = this.getRecipies()
+  }
 
   ngOnInit(): void {
   }
 
+  getRecipies(): Recipe[] {
+    return [
+      new Recipe("making bread", "bread!", [
+        "the",
+        "steps",
+        "for",
+        "making",
+        "bread"
+      ]),
+      new Recipe("making apple pie", "even more yummy!", [
+        "the",
+        "steps",
+        "for",
+        "making",
+        "apple",
+        "pie"
+      ])
+    ]
+  }
 }
+

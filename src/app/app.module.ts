cb4aefa39272caf5881e36cc3dc508e893fa50d7
David <david@pop-os.localdomain>
2021-07-20 19:56:35 +0200

    recfactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
-import { ShoppingListComponentComponent } from './shopping-list-component/shopping-list-component.component';
-import { RecipeBookComponentComponent } from './recipe-book-component/recipe-book-component.component';
-import { ShoppingListEditComponentComponent } from './shopping-list-edit-component/shopping-list-edit-component.component';
-import { RecipeListComponentComponent } from './recipe-list-component/recipe-list-component.component';
-import { RecipeItemComponentComponent } from './recipe-item-component/recipe-item-component.component';
-import { RecipeDetailComponentComponent } from './recipe-detail-component/recipe-detail-component.component';
+import { ShoppingListComponentComponent } from './shoppinglist/shopping-list-component/shopping-list-component.component';
+import { RecipeBookComponentComponent } from './recipebook/recipe-book-component/recipe-book-component.component';
+import { ShoppingListEditComponentComponent } from './shoppinglist/shopping-list-edit-component/shopping-list-edit-component.component';
+import { RecipeListComponentComponent } from './recipebook/recipe-list-component/recipe-list-component.component';
+import { RecipeItemComponentComponent } from './recipebook/recipe-item-component/recipe-item-component.component';
+import { RecipeDetailComponentComponent } from './recipebook/recipe-detail-component/recipe-detail-component.component';
 
 @NgModule({
   declarations: [

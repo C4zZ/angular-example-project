4ad2e470eb5af9471a2d2b4015d43006a6244d41
David <david@pop-os.localdomain>
2021-07-20 19:19:04 +0200

    add ShoppingListComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
+import { ShoppingListComponentComponent } from './shopping-list-component/shopping-list-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponentComponent
+    HeaderComponentComponent,
+    ShoppingListComponentComponent
   ],
   imports: [
     BrowserModule

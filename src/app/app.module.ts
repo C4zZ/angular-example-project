6ca52be65fdce601e5f0e56da61313672b1e79e4
David <david@pop-os.localdomain>
2021-07-21 18:10:42 +0200

    move components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shoppinglist/shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shoppinglist/shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponentComponent } from './recipebook/recipe-list/recipe-list-component.component';
-import { RecipeItemComponent } from './recipebook/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipebook/recipe-detail/recipe-detail.component';
+import { RecipeListComponentComponent } from './recipies/recipe-list/recipe-list-component.component';
+import { RecipeItemComponent } from './recipies/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [

236d875dc6524c0d7830a2081ea96546a160e7b0
David <david@pop-os.localdomain>
2021-07-21 18:03:44 +0200

    delete RecipeBook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shoppinglist/shopping-list/shopping-list.component';
-import { RecipeBookComponent } from './recipebook/recipe-book/recipe-book.component';
 import { ShoppingListEditComponent } from './shoppinglist/shopping-list-edit/shopping-list-edit.component';
 import { RecipeListComponentComponent } from './recipebook/recipe-list/recipe-list-component.component';
 import { RecipeItemComponent } from './recipebook/recipe-item/recipe-item.component';
@@ -15,7 +14,6 @@ import { RecipeDetailComponent } from './recipebook/recipe-detail/recipe-detail.
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    RecipeBookComponent,
     ShoppingListEditComponent,
     RecipeListComponentComponent,
     RecipeItemComponent,

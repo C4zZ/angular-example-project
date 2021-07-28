060f3db64553695d55c8e0d58384b9a1318e3203
David <david@pop-os.localdomain>
2021-07-28 19:40:36 +0200

    implemented

--- src/app/shoppinglist/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shoppinglist/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import {Component, ElementRef, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from "../../shared/Ingredient";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('IngredientName')
+  ingredientNameRef: ElementRef;
+  @ViewChild('IngredientAmount')
+  ingredientAmountRef: ElementRef;
+  @Output()
+  ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddIngredient(){
+    let ingredientToAdd = new Ingredient(
+      this.ingredientNameRef.nativeElement.value,
+      this.ingredientAmountRef.nativeElement.value
+    );
+    this.ingredientAdded.emit(ingredientToAdd);
   }
 
 }

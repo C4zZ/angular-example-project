74e97777490c3959f06983ccd4ad5abd12264896
David <david@pop-os.localdomain>
2021-07-28 18:13:48 +0200

    listening to showdetails event

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { EventEmitter } from '@angular/core';
+import {EventEmitter, Output} from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import {Recipe} from "./recipe-item/recipe";
 
@@ -8,6 +8,7 @@ import {Recipe} from "./recipe-item/recipe";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output()
   recipeClicked = new EventEmitter<Recipe>();
   recipes: Recipe[];
 
